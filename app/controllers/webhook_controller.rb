bd42f2f963dbbc74a6ad9131d75a551afc285f44
pokohide <hyde31415@gmail.com>
2016-08-24 15:57:08 +0900

    inspectでdebug

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -28,7 +28,7 @@ class WebhookController < ApplicationController
       error 400 do 'Bad Request' end
     end
     receive_request = Line::Bot::Receive::Request.new(request.env)
-    Rails.logger.debug(receive_request)
+    Rails.logger.debug(receive_request.inspect)
     receive_request.data.each do |message|
       c = LineClient.new(client, message)
       c.reply
--- lib/line_client.rb
+++ lib/line_client.rb
@@ -57,12 +57,12 @@ class LineClient
       case @message.content
       when Line::Bot::Message::Text
         Rails.logger.debug(@message.content[:text])
-        Rails.logger.debug(@client)
+        Rails.logger.debug(@client.inspect)
         x = @client.send_text(
           to_mid: @to_mid,
           text: @message.content[:text]
         )
-        Rails.logger.debug(x)
+        Rails.logger.debug(x.inspect)
       when Line::Bot::Message::Sticker
         @client.send_text(
           to_mid: @to_mid,
