47054fb11d0aab83d28b0ddede06026bd5dd5522
pokohide <hyde31415@gmail.com>
2016-08-24 17:02:57 +0900

    レシピモデル追加

---
+++ db/migrate/20160824080228_create_recipes.rb
@@ -0,0 +1,12 @@
+class CreateRecipes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :recipes do |t|
+      t.string :image
+      t.string :name
+      t.string :rid
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
