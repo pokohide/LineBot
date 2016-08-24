edcebaa6903267e7ef91363ee4a4f78c49e91593
pokohide <hyde31415@gmail.com>
2016-08-24 15:03:54 +0900

    bug fixed

--- lib/line_client.rb
+++ lib/line_client.rb
@@ -54,7 +54,7 @@ class LineClient
         )
       end
     when Line::Bot::Receive::Message
-      case message.content
+      case @message.content
       when Line::Bot::Message::Text
         @client.send_text(
           to_mid: @to_mid,
