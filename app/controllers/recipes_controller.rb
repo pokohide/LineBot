616d7cf8cb5eaedd5eb35781b79eea955ba924f3
pokohide <hyde31415@gmail.com>
2016-08-25 16:36:15 +0900

    レシピの手順のwebviewを作成

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,12 @@
 class RecipesController < ApplicationController
   include ActionView::Helpers::OutputSafetyHelper
+  require 'erb'
+  
   def show
     @recipe = Recipe.find_by(rid: params[:rid])
-    
-    render text: erb('show')
+    @steps = @recipe.steps.map{|s| [s.turn.to_i+1, s.image, s.content]}
+    #render html: erb('materials', {title: @recipe.name, portion: @recipe.portion, image: @recipe.image, time: @recipe.time, fee: @recipe.fee, materials: @materials}), layout: false
+    render html: erb('show', {title: @recipe.name, portion: @recipe.portion, image: @recipe.image, time: @recipe.time, fee: @recipe.fee, steps: @steps}), layout: false
   end
 
   def materials
@@ -15,14 +18,14 @@ class RecipesController < ApplicationController
 
   private
   def erb(fname, opts)
-    require 'erb'
     html = <<TEXT_END
+      <% steps = #{opts[:steps] || []} %>
       <% title = '#{opts[:title]}' %>
       <% portion = '#{opts[:portion]}' %>
       <% image = '#{opts[:image]}' %>
       <% fee = '#{opts[:fee]}' %>
       <% time = '#{opts[:time]}' %>
-      <% materials = #{opts[:materials]} %>
+      <% materials = #{opts[:materials] || []} %>
 TEXT_END
     html += File.open("#{Rails.root}/app/views/recipes/#{fname}.html.erb", 'r').read
     ERB.new(html).result.html_safe
