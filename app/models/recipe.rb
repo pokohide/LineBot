c292c297450f3dda7abb3217f7a5dc4cb8027ae5
pokohide <hyde31415@gmail.com>
2016-08-25 12:01:38 +0900

    紐付けok

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
-class Recipe < ApplicationRecord
+class Recipe < ApplicationRecord  
+  has_many :materials
+  has_many :steps
   scope :like, -> (keyword) { where("name like ? or description like ?", "%#{keyword}%", "%#{keyword}%") }
 end
