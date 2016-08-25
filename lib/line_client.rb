012ca6b800db3a76f72793c96f13c769b9362665
pokohide <hyde31415@gmail.com>
2016-08-25 18:35:56 +0900

    作るの開始ボタンを追加

--- lib/line_client.rb
+++ lib/line_client.rb
@@ -38,55 +38,34 @@ class LineClient
     when Line::Bot::Receive::Operation
       case data.content
       when Line::Bot::Operation::AddedAsFriend
-        @client.send_text(
-          to_mid: @to_mid,
-          text: """
-            料理bot登録してくれてありがとう！􀁺
-            料理上手への道の第一歩を踏み出したそこのあなた􀁸
-            これから一緒に料理を作っていって、料理レベルを上げていこう！✨
-            上手くできたら友達に自慢できるかも？！􀂌
-          """
-        ) 
+        introduce_myself
       end
     when Line::Bot::Receive::Message
       if @user.cooking?
         case @message.content
         when Line::Bot::Message::Text
-          @client.send_text(
-            to_mid: @to_mid,
-            text: '質問ですか'
-          )
+          send_text '質問ですか?'
         when Line::Bot::Message::Sticker
-          @client.send_text(
-            to_mid: @to_mid,
-            text: """
-              料理bot登録してくれてありがとう！􀁺
-              料理上手への道の第一歩を踏み出したそこのあなた􀁸
-              これから一緒に料理を作っていって、料理レベルを上げていこう！✨
-              上手くできたら友達に自慢できるかも？！􀂌
-            """
-          )        
+          send_text 'ステッカー送ってきたな！'      
         end
       else
         case @message.content
         when Line::Bot::Message::Text
-          recipes = Recipe.like(@message.content[:text])
-          if recipes.count == 0
-            @client.send_text(
-              to_mid: @to_mid,
-              text: '見つかりませんでした。'
-            )
+          if /(.+?)をつくります！！！/ =~ @message.content[:text]
+            send_text "#{$1}のクッキングを開始します！"
           else
-            3.times do |i|
-              send_recipe recipes[i]
-              send_choice recipes[i]
+            recipes = Recipe.like(@message.content[:text])
+            if recipes.count == 0
+              send_text '見つかりませんでした。'
+            else
+              3.times do |i|
+                send_recipe recipes[i]
+                send_choice recipes[i]
+              end
             end
           end
         when Line::Bot::Message::Sticker
-          @client.send_text(
-            to_mid: @to_mid,
-            text: 'okok'
-          )        
+          send_text 'okok'      
         end
       end
     end 
@@ -116,7 +95,7 @@ class LineClient
       },
       COOK: {
         text: 'つくる',
-        params_text: "#{recipe.name}をつくる",
+        params_text: "#{recipe.name}をつくります！！！",
         type: "sendMessage",
       }
     ).add_listener(
@@ -144,123 +123,6 @@ class LineClient
     )
   end
 
-  def reply1(line_ids, keyword)
-    crawler = Crawler.new(keyword)
-    crawler.scrape
-    3.times do |i|
-      sent_recipe(line_ids, crawler.results[i])
-      #rich_message(line_ids, crawler.results[i])
-      #send_image(line_ids, crawler.results[i][:image])
-      #send_text(line_ids, crawler.results[i][:content])
-      rich_message(line_ids, crawler.results[i])
-    end
-  end
-
-  def send_text(line_ids, message)
-    post('/v1/events', {
-        to: line_ids,
-        content: {
-            contentType: ContentType::TEXT,
-            toType: ToType::USER,
-            text: message
-        },
-        toChannel: TO_CHANNEL,
-        eventType: EVENT_TYPE
-    })
-  end
-
-  def send_image(line_ids, image)
-    post('/v1/events', {
-      to: line_ids,
-      content: {
-        contentType: ContentType::IMAGE,
-        toType: ToType::USER,
-        originalContentUrl: image,
-        previewImageUrl: image
-      },
-      toChannel: TO_CHANNEL,
-      eventType: EVENT_TYPE
-    })
-  end
-
-  def rich_message(line_ids, recipe)
-    json = {
-      canvas: {
-        width: 1040,
-        height: 1040,
-        initialScene: 'scene1'
-      },
-      images: {
-        images1: {
-          x: 0,
-          y: 0,
-          w: 1040,
-          h: 1040
-        }
-      },
-      actions: {
-        open: {
-          type: 'web',
-          params: {
-            linkUri: "https://line2016.herokuapp.com/api/choice?mid=#{line_ids.first}&recipe_id=#{recipe[:id]}"
-          }
-        },
-        yes: {
-          type: 'web',
-          params: {
-            linkUri: 'https://www.google.co.jp/#q=yes'
-          }
-        },
-        no: {
-          type: 'web',
-          params: {
-            linkUri: 'https://www.google.co.jp/#q=no'
-          }
-        }
-      },
-      scenes: {
-        scene1: {
-          listeners: [
-            {
-              type: 'touch',
-              action: 'no',
-              params: [0, 0, 520, 1040]
-            },
-            {
-              type: 'touch',
-              action: 'yes',
-              params: [520, 0, 520, 1040]
-            }
-          ],
-          draws: [
-            {
-              x: 0,
-              y: 0,
-              w: 1040,
-              h: 1040,
-              image: 'image1'
-            }
-          ]
-        }
-      }
-    }.to_json
-    Rails.logger.info({success: json})
-    post('/v1/events', {
-      to: line_ids,
-      content: {
-        contentType: ContentType::RICH,
-        toType: ToType::USER,
-        contentMetadata: {
-          #DOWNLOAD_URL: recipe[:image],
-          DOWNLOAD_URL: 'https://line2016.herokuapp.com/images',
-          SPEC_REV: '1',
-          ALT_TEXT: recipe[:content],
-          MARKUP_JSON: json
-        }
-      }
-    })
-  end
-
   def post(path, data)
     client = Faraday.new(:url => END_POINT) do |conn|
       conn.request :json
@@ -280,5 +142,25 @@ class LineClient
       request.body = data
     end
     res
+  end
+
+  private
+  def introduce_myself
+    @client.send_text(
+      to_mid: @to_mid,
+      text: """
+        料理bot登録してくれてありがとう！􀁺
+        料理上手への道の第一歩を踏み出したそこのあなた􀁸
+        これから一緒に料理を作っていって、料理レベルを上げていこう！✨
+        上手くできたら友達に自慢できるかも？！􀂌
+      """
+    )
+  end
+
+  def send_text text 
+    @client.send_text(
+      to_mid: @to_mid,
+      text: text
+    )   
   end
 end
