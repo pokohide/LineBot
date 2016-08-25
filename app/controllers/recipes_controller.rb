5ba2712d5e720f691243307fac04c15da1457781
pokohide <hyde31415@gmail.com>
2016-08-25 19:58:34 +0900

    スタイルの変なところを修正

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,7 +5,6 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find_by(rid: params[:rid])
     @steps = @recipe.steps.map{|s| [s.turn.to_i+1, s.image, s.content]}
-    #render html: erb('materials', {title: @recipe.name, portion: @recipe.portion, image: @recipe.image, time: @recipe.time, fee: @recipe.fee, materials: @materials}), layout: false
     render html: erb('show', {title: @recipe.name, portion: @recipe.portion, image: @recipe.image, time: @recipe.time, fee: @recipe.fee, steps: @steps}), layout: false
   end
 
